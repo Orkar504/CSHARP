f833c6d8dfeab2b84d32026b6647d78f2fb1d72c
Orkar504 <jose7211963@gmail.com>
2023-08-31 00:45:56 -0600

    comentarios

--- Salidas.cs
+++ Salidas.cs
@@ -7,8 +7,8 @@ namespace CSHARP
     {
         public void salida()
         {
-            Console.WriteLine("En este se salta la linea");
-            Console.Write("Este no salta la linea");
+            Console.WriteLine("En este se salta la linea"); // Este salta linea cada vez 
+            Console.Write("Este no salta la linea"); //Write no salta lineas 
             Console.Write("  Ejemplo");
         }
     }
